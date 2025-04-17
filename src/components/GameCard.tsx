db867f9d81f99ba021f313037046dbf410b9f6b0
awaasolution <awaasolution@gmail.com>
2025-04-17 14:21:09 +0630

    show loading skelton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ interface Prop {
 const GameCard = ({ game }: Prop) => {
   console.log(game.parent_platforms);
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card borderRadius={10} width={"300px"} overflow={"hidden"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
 import { Image, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
-
+import GameCardSkelton from "./GameCardSkelton";
 
 const GameGrid = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
+  const skeltons = [1, 2, 3, 4, 5, 6];
   console.log("games");
   return (
     <>
@@ -13,6 +14,8 @@ const GameGrid = () => {
         padding={10}
         spacing={10}
       >
+        {isLoading &&
+          skeltons.map((skelton) => <GameCardSkelton key={skelton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
